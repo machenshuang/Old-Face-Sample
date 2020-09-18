c08194dbd064e7a64842199699f385850ac2e997
machenshuang <machenshuang@yy.com>
2020-09-18 20:45:15 +0800

    初始化项目

---
+++ Old Face Sample/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Old Face Sample
+//
+//  Created by 马陈爽 on 2020/9/18.
+//  Copyright © 2020 马陈爽. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Old Face Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Old Face Sample
+//
+//  Created by 马陈爽 on 2020/9/18.
+//  Copyright © 2020 马陈爽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
