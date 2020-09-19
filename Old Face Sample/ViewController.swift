1ead9007a339ee80a6af7f21ff712b84c3b878f8
machenshuang <machenshuang@yy.com>
2020-09-19 11:00:29 +0800

    feat：加入GPUImage的相机捕获。

--- Old Face Sample/ViewController.swift
+++ Old Face Sample/ViewController.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 import GPUImage
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var renderView: RenderView!
+    @IBOutlet weak var sliderView: UISlider!
+    
+    private var camera: Camera!
+    private var effectOperation: BrightnessAdjustment!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.effectOperation = BrightnessAdjustment()
+        do {
+            self.camera = try Camera(sessionPreset: .photo)
+            self.camera.runBenchmark = true
+            self.camera --> self.effectOperation --> self.renderView
+            self.camera.startCapture()
+        } catch {
+            fatalError("Could not initialize rendering pipeline: \(error)")
+        }
+        
+        
     }
 
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        self.effectOperation.brightness = sender.value;
+    }
+    
+    
+}
 
+extension ViewController: CameraDelegate {
+    func didCaptureBuffer(_ sampleBuffer: CMSampleBuffer) {
+        
+    }
 }
 
